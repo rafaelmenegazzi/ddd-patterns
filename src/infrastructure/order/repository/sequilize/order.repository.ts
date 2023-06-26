41003c6cdc1975eacf643cc8fa93e68668d1e191
rafaelmenegazzi <rafael.menegazzi.usp@gmail.com>
2023-06-25 21:09:43 -0300

    OrderRepository extends OrderRepositoryInterface

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,9 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -21,5 +22,17 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    throw new Error ('not implemented')
+  }
+
+  async find(id: string): Promise<Order> {
+    throw new Error ('not implemented')
+  }
+
+  async findAll(): Promise<Order[]> {
+    throw new Error ('not implemented')
   }
 }

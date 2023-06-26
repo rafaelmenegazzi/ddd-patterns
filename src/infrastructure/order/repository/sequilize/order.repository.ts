f7977c4b29c189a87fb8970f9f18d15868abafc2
rafaelmenegazzi <rafael.menegazzi.usp@gmail.com>
2023-06-25 21:11:14 -0300

    add OrderRepository.update

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -25,7 +25,39 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async update(entity: Order): Promise<void> {
-    throw new Error ('not implemented')
+    const order = await OrderModel.update(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      },
+    )
+
+    if (order?.[0]) {
+      const itemsPromises = entity.items.map(item =>
+        OrderItemModel.upsert({
+          id: item.id,
+          name: item.name,
+          order_id: entity.id,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })
+      )
+      await Promise.all(itemsPromises)
+    }
   }
 
   async find(id: string): Promise<Order> {
